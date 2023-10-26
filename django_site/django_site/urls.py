996477c8c39a210e3660a25eb1ccd88a010f6ae4
tap-tap-bjj <transstandart39@gmail.com>
2023-10-26 17:30:33 +0200

    lesson 1.4

--- django_site/django_site/urls.py
+++ django_site/django_site/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from women.views import index
+from django.urls import path, include
+from women import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', index)
+    path('', include('women.urls')), #http://127.0.0.1:8000/
+    path('cats/', views.categories) #http://127.0.0.1:8000/cats/
 ]
--- django_site/women/views.py
+++ django_site/women/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(reqest):
+def index(request):
     return HttpResponse("Страница приложения women.")
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")
