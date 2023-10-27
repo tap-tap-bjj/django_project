d664f19f4bda4e60d3c3cae1063e6381792222d5
tap-tap-bjj <transstandart39@gmail.com>
2023-10-27 23:44:43 +0200

    lesson 1.6

--- django_site/women/urls.py
+++ django_site/women/urls.py
@@ -1,8 +1,14 @@
 
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
+
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index), #http://127.0.0.1:8000/
-    path('cats/', views.categories) #http://127.0.0.1:8000/cats/
+    path('cats/<int:cat_id>/', views.categories), #http://127.0.0.1:8000/cats/int/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug), #http://127.0.0.1:8000/cats/slug
+    path("archive/<year4:year>/", views.archiive),
 ]
--- django_site/women/views.py
+++ django_site/women/views.py
@@ -5,5 +5,13 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Страница приложения women.")
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archiive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
