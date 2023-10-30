47385aa7ed91f02359ca04806719b342b9fda930
tap-tap-bjj <transstandart39@gmail.com>
2023-10-30 23:14:43 +0200

    lesson 1.9

--- django_site/women/urls.py
+++ django_site/women/urls.py
@@ -7,9 +7,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index), #http://127.0.0.1:8000/
-    path('cats/<int:cat_id>/', views.categories), #http://127.0.0.1:8000/cats/int/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug), #http://127.0.0.1:8000/cats/slug
-    path("archive/<year4:year>/", views.archiive),
+    path('', views.index, name='home'), #http://127.0.0.1:8000/
+    path('cats/<int:cat_id>/', views.categories, name='cat_id'), #http://127.0.0.1:8000/cats/int/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'), #http://127.0.0.1:8000/cats/slug
+    path("archive/<year4:year>/", views.archiive, name='archaive'),
 ]
 
--- django_site/women/views.py
+++ django_site/women/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 # Create your views here.
 def index(request):
@@ -15,9 +17,12 @@ def categories_by_slug(request, cat_slug):
 
 def archiive(request, year):
     if year > 2023:
-        raise Http404
+        uri = reverse('cats', args=('music', ))
+        return HttpResponseRedirect(uri)
+
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Сираница не найдена</h1>")
+
