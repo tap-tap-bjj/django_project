dc41844fff27edbd19d0abc38b77c44c3c79bb6a
tap-tap-bjj <transstandart39@gmail.com>
2023-10-31 23:41:11 +0200

    lesson 2.2

--- django_site/women/urls.py
+++ django_site/women/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path, re_path, register_converter
 from . import views
 from . import converters
@@ -7,9 +6,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index, name='home'), #http://127.0.0.1:8000/
-    path('cats/<int:cat_id>/', views.categories, name='cat_id'), #http://127.0.0.1:8000/cats/int/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'), #http://127.0.0.1:8000/cats/slug
-    path("archive/<year4:year>/", views.archiive, name='archaive'),
+    path('', views.index, name='home'),  # http://127.0.0.1:8000
+    path('about/', views.about, name='about'),
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'), # http://127.0.0.1:8000/cats/2/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/cats/fdgdg/
+    path("archive/<year4:year>/", views.archive, name='archive'),
 ]
-
--- django_site/women/views.py
+++ django_site/women/views.py
@@ -1,28 +1,53 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Страница приложения women.")
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+def index(request): # HttpRequest
+    # t = render_to_string('women/index.html')
+    # return HttpResponse(t)
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'float': 28.56,
+        'lst': [1, 2, 'abc', True],
+        'set': {1, 1, 2, 3, 2, 5},
+        'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+        'obj': MyClass(10, 20),
+    }
+    return render(request, 'women/index.html', context=data)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'title': 'О сайте'})
+
 
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
 
 
 def categories_by_slug(request, cat_slug):
+    if request.POST:
+        print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
 
 
-def archiive(request, year):
+def archive(request, year):
     if year > 2023:
-        uri = reverse('cats', args=('music', ))
-        return HttpResponseRedirect(uri)
+        uri = reverse('cats', args=('sport', ))
+        return HttpResponsePermanentRedirect(uri)
 
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 
 def page_not_found(request, exception):
-    return HttpResponseNotFound("<h1>Сираница не найдена</h1>")
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
 
