9b1510e2462854b0ec1577c5620595cb3d709b17
tap-tap-bjj <transstandart39@gmail.com>
2023-11-06 22:21:47 +0200

    lesson 2.5

--- django_site/women/views.py
+++ django_site/women/views.py
@@ -5,7 +5,11 @@ from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -27,25 +31,21 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
-
-
-def categories_by_slug(request, cat_slug):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
-
-
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport', ))
-        return HttpResponsePermanentRedirect(uri)
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
-
-
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
